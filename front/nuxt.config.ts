341b6abdd7dd9b8f6a5db7effca4fd703b1955bb
Ark21bit <webleif@vk.com>
2023-06-02 14:10:14 +0300

    update

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -17,15 +17,7 @@ export default defineNuxtConfig({
     }, 
     strapi: {
         devtools: true,
-        url: 'http://127.0.0.1:1337'
-    },
-    runtimeConfig:{
-        public:{
-            baseURL: process.env.NODE_ENV === 'dev' ? "http://127.0.0.1:1337/api" : "https://skynet-h2gp.onrender.com/api",
-            strapi: {
-                url: process.env.NODE_ENV === 'dev' ? "http://127.0.0.1:1337" : "https://skynet-h2gp.onrender.com"
-            },
-        }
+        url: process.env.NODE_ENV === 'dev' ? "http://127.0.0.1:1337" : "https://skynet-h2gp.onrender.com"
     },
     googleFonts: {
         families: {
